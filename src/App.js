75787ebba103c8139735c09a08b888aa77cba41e
Michael Simini <msimini@br.ibm.com>
2019-10-27 10:59:25 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+import TechList from './components/TechList';
+
+// import profile from './assets/profile.png';
+
+function App(){
+  return <TechList/>
+  // return <img width="200" src={profile}></img>
+}
+
+export default App;
